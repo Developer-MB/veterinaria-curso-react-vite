a283617878ef12daf88e4f2fa7a256e63069f45a
YisusCrowley <antoncrauli@gmail.com>
2022-01-25 13:47:48 -0700

    Segundo commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 function Header() {
     return (
-        <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes {""} 
-            <span className="text-indigo-700">Veterinaria</span>
-        </h1>
+        <div>
+            <h1 className="font-black text-6xl text-center md:w-2/3 mx-auto">
+                <span className="text-green-700">Curso </span>
+                <span className="text-blue-800"> React</span>
+            </h1>
+            <h2 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+                Seguimiento Pacientes {""}
+                <span className="text-indigo-700">Veterinaria</span>
+            </h2>
+        </div>
     )
 }
 
